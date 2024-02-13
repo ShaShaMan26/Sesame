478e1d45d989553606ddfba50081aad5c9c06a9a
Cristiano Porretta <shashaman26@icloud.com>
2024-02-13 15:31:38 -0500

    worked-on-obj

--- src/main/java/Mine/Canvas.java
+++ src/main/java/Mine/Canvas.java
@@ -2,9 +2,11 @@ package Mine;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class Canvas extends JPanel {
     private Color bgColor = Color.WHITE;
+    private BufferedImage bgImg = null;
     public static Dimension defDim = new Dimension(640, 480);
 
     public Canvas() {
@@ -14,16 +16,27 @@ public class Canvas extends JPanel {
     public void setBgColor(Color bgColor) {
         this.bgColor = bgColor;
     }
+    public void setBgImg(BufferedImage bgImg) {
+        this.bgImg = bgImg;
+    }
 
     public Color getBgColor() {
         return bgColor;
     }
+    public BufferedImage getBgImg() {
+        return bgImg;
+    }
 
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
-
         g.setColor(bgColor);
         g.fillRect(0, 0, getWidth(), getHeight());
+
+        // make scale <3
+        g.drawImage(bgImg, 0, 0, 640, 480, null);
+
+        for (Component c : getComponents()) {
+            c.paint(g);
+        }
     }
 }
--- src/main/java/Mine/Objects/Obj.java
+++ src/main/java/Mine/Objects/Obj.java
@@ -1,6 +1,69 @@
 package Mine.Objects;
 
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
-public class Obj extends Component {
+public abstract class Obj extends Component {
+    protected BufferedImage sprite;
+    protected Rectangle hitBox = new Rectangle(0, 0, 0, 0);
+    protected boolean hitBoxVisible = false;
+
+    protected Obj() {
+    }
+    protected Obj(int x, int y) {
+        this();
+        setLocation(x, y);
+    }
+    protected Obj(BufferedImage sprite) {
+        this();
+        setSprite(sprite);
+    }
+    protected Obj(int x, int y, BufferedImage sprite) {
+        this(x, y);
+        setSprite(sprite);
+    }
+
+    public boolean collidingWith(Obj obj) {
+        Rectangle thisHitBox = (Rectangle) hitBox.clone();
+        thisHitBox.setLocation(getX() + hitBox.x, getY() + hitBox.y);
+
+        Rectangle themHitBox = (Rectangle) obj.getHitBox().clone();
+        themHitBox.setLocation(obj.getX() + themHitBox.x, obj.getY() + themHitBox.y);
+
+        return thisHitBox.intersects(themHitBox);
+    }
+
+    public void setSprite(BufferedImage sprite) {
+        this.sprite = sprite;
+        hitBox.setSize(sprite.getWidth(), sprite.getHeight());
+    }
+    public void setHitBox(int xOffset, int yOffset, int width, int height) {
+        hitBox = new Rectangle(xOffset, yOffset, width, height);
+    }
+    public void setHitBox(Rectangle hitBox) {
+        this.hitBox = hitBox;
+    }
+    public void setHitBoxVisible(boolean hitBoxVisible) {
+        this.hitBoxVisible = hitBoxVisible;
+    }
+
+    public BufferedImage getSprite() {
+        return sprite;
+    }
+    public Rectangle getHitBox() {
+        return hitBox;
+    }
+    public boolean isHitBoxVisible() {
+        return hitBoxVisible;
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(sprite, getX(), getY(), null);
+
+        if (hitBoxVisible) {
+            g.setColor(Color.GREEN);
+            g.drawRect(hitBox.x + getX(), hitBox.y + getY(), hitBox.width, hitBox.height);
+        }
+    }
 }
